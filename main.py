d79fadbbc274a0dc6e6aab85b19370a0a61210c4
Jonshea Edwards <Jonshea23@gmail.com>
2024-10-02 22:29:45 -0400

    First Commit

--- main.py
+++ main.py
@@ -3,7 +3,12 @@
 # Output: Your current age followed by your age in 2050
 
 # Create your variables here
+myCurrentAge = 19  
+currentYear = 2024  
 
+# Calculate your age in 2050 using the defined variables
 myNewAge = myCurrentAge + (2050 - currentYear)
+
+# Output the results using the variables
 print("My Current Age is " + str(myCurrentAge))
 print("I will be " + str(myNewAge) + " in 2050.")
